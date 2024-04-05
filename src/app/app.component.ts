e647c56f8f4a54dd906489dc6d878cb4a189aed6
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-04-05 16:02:28 +0530

    Book Class Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
-
+class abc {
+  constructor() {
+    console.log('abc constructor called!! ');
+  }
+}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +15,7 @@ import { EmployeeComponent } from './employee/employee.component';
 })
 export class AppComponent {
   title = 'angular-series';
+  constructor() {
+    console.log('AppComponent Constructor Called!!');
+  }
 }
